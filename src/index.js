253c5ace122b5d07e4533bbaa57d11154b11573d
pnievsky <pniewski.b@o2.pl>
2020-12-25 10:42:21 +0100

    Refactoring Data Loading to Lifecycle Methods

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ class App extends React.Component {
 		// this is the only time! we do direct assignment
 		// to this.state!
 		this.state = { lat: null, errorMessage: '' };
-
-		window.navigator.geolocation.getCurrentPosition(
-		(position) => {
-			// we called setstate!
-			this.setState({ lat: position.coords.latitude });
-		},
-		err => {
-			this.setState ({ errorMessage: err.message })}
-		);
 	}
+
+componentDidMount() {
+	window.navigator.geolocation.getCurrentPosition(
+	(position) => this.setState({ lat: position.coords.latitude }),
+	err => this.setState ({ errorMessage: err.message })
+	);
+}
 
 // React says we have to define render!
 	render() {

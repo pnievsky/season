656d1d5f00a24a3ee488c43858bc972a4f3cc43d
pnievsky <pniewski.b@o2.pl>
2020-12-25 10:14:17 +0100

    Conditionally Rendering Content

--- src/index.js
+++ src/index.js
@@ -6,20 +6,29 @@ class App extends React.Component {
 		super(props);
 		// this is the only time! we do direct assignment
 		// to this.state!
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: '' };
 
 		window.navigator.geolocation.getCurrentPosition(
 		(position) => {
 			// we called setstate!
 			this.setState({ lat: position.coords.latitude });
 		},
-		(err) => console.log(err )
+		err => {
+			this.setState ({ errorMessage: err.message })}
 		);
 	}
 
 // React says we have to define render!
-	render(){
-		return <div>Latitude: {this.state.lat} </div>;
+	render() {
+	if (this.state.errorMessage && !this.state.lat) {
+		return <div>Error: {this.state.errorMessage}</div>
+	}
+
+	if (!this.state.errorMessage && this.state.lat) {
+		return <div>Latitude: {this.state.lat}</div>
+	}
+
+	return <div>Loading!</div>;
 	}
 }
 

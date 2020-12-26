6914e7b80d5be1ef34c1ea44b112bea7002c3be9
pnievsky <pniewski.b@o2.pl>
2020-12-26 10:38:21 +0100

    Alternate State Initialization Passing State as Props Determining Season Ternary Expressions in JSX Showing Icons Extracting Options to Config Objects Adding Some Styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
 	state = { lat: null, errorMessage: ''};
@@ -18,7 +20,7 @@ componentDidMount() {
 	}
 
 	if (!this.state.errorMessage && this.state.lat) {
-		return <div>Latitude: {this.state.lat}</div>
+		return <SeasonDisplay lat={this.state.lat} />
 	}
 
 	return <div>Loading!</div>;
